668185ac5abb872cb376e1fac2eba81d32328164
Khusrav Ashurzoda <khusrav2000@mail.ru>
2019-09-26 22:27:34 +0300

    Add pick foods list.

--- app/src/main/java/com/time2raise/customer/PickFoodsByCategoryIdFragment.java
+++ app/src/main/java/com/time2raise/customer/PickFoodsByCategoryIdFragment.java
@@ -2,9 +2,12 @@ package com.time2raise.customer;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +20,7 @@ import com.time2raise.customer.data.NetworkClient;
 import com.time2raise.customer.data.apis.Customer;
 import com.time2raise.customer.data.model.EventDetailed;
 import com.time2raise.customer.data.model.EventInf;
+import com.time2raise.customer.data.model.FoodSize;
 import com.time2raise.customer.data.model.FoodSizesInformation;
 import com.time2raise.customer.data.model.ResFood;
 
@@ -122,31 +126,91 @@ public class PickFoodsByCategoryIdFragment extends Fragment {
     }
 
     private void startShowFoodsToPick(List<ResFood> resFoods) {
+
+        int i = 0 ;
         for (ResFood resFood : resFoods){
-            RelativeLayout food = new RelativeLayout(getContext());
+            i ++ ;
+            final LinearLayout food = new LinearLayout(getContext());
 
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.WRAP_CONTENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT
             );
 
+            Resources r = getContext().getResources();
+            int left = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    20,
+                    r.getDisplayMetrics()
+            );
+
+            int top = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    10,
+                    r.getDisplayMetrics()
+            );
+
+            params.setMargins(left, top , 0 , 0 );
             food.setLayoutParams(params);
+            food.setOrientation(LinearLayout.VERTICAL);
+
 
 
 
             TextView foodName = new TextView(getContext());
 
-            RelativeLayout.LayoutParams foodNameParams = new RelativeLayout.LayoutParams(
+            LinearLayout.LayoutParams foodNameParams = new LinearLayout.LayoutParams(
                     RelativeLayout.LayoutParams.WRAP_CONTENT,
                     RelativeLayout.LayoutParams.WRAP_CONTENT
             );
 
             foodName.setLayoutParams(foodNameParams);
             foodName.setText(resFood.getFoodName());
+            foodName.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+            foodName.setTextColor(getContext().getColor(R.color.food_list_color));
+            foodName.setCompoundDrawablesWithIntrinsicBounds(null, null , getContext().getDrawable(R.drawable.icon_polygon), null);
+            foodName.setGravity(Gravity.CENTER_VERTICAL);
+            int drawablePadding = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    5,
+                    r.getDisplayMetrics()
+            );
+            foodName.setCompoundDrawablePadding(drawablePadding);
 
             food.addView(foodName);
 
-            TextView foodPrice = new TextView(getContext());
+            final LinearLayout foodSizes = new LinearLayout(getContext());
+
+            final LinearLayout.LayoutParams foodSizeParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            foodSizes.setLayoutParams(foodSizeParams);
+
+            for (FoodSizesInformation foodSize : resFood.getFoodSizesInformation()){
+                TextView foodSizeName = new TextView(getContext());
+                foodSizeName.setText(foodSize.getFoodSizes().getSizeName());
+                foodSizes.addView(foodSizeName);
+            }
+
+            //foodSizes.setEnabled(false);
+
+            foodName.setId(i + 1);
+
+            foodName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.out.println("Ebabled!");
+                    if (foodSizes.getVisibility() == View.VISIBLE)
+                        foodSizes.setVisibility(View.GONE);
+                    else foodSizes.setVisibility(View.VISIBLE);
+                }
+            });
+
+            food.addView(foodSizes);
+
+            /*TextView foodPrice = new TextView(getContext());
 
             RelativeLayout.LayoutParams foodPriceParams = new RelativeLayout.LayoutParams(
                     RelativeLayout.LayoutParams.WRAP_CONTENT,
@@ -161,9 +225,22 @@ public class PickFoodsByCategoryIdFragment extends Fragment {
 
             foodPrice.setText("+ $" + foodSizesInformation.get(0).getPrice());
 
-            food.addView(foodPrice);
+            food.addView(foodPrice);*/
+
+            View line = new View(getContext());
+
+            LinearLayout.LayoutParams lineParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    getContext().getResources().getDimensionPixelOffset(R.dimen.line_height)
+            );
+
+            lineParams.setMargins(left, top, 0 , 0);
+
+            line.setLayoutParams(lineParams);
+            line.setBackgroundColor(getContext().getColor(R.color.line_color));
 
             layoutFoodsList.addView(food);
+            layoutFoodsList.addView(line);
 
         }
     }
--- app/src/main/java/com/time2raise/customer/data/model/FoodSize.java
+++ app/src/main/java/com/time2raise/customer/data/model/FoodSize.java
@@ -3,7 +3,7 @@ package com.time2raise.customer.data.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-class FoodSize {
+public class FoodSize {
     @SerializedName("FoodSizeId")
     @Expose
     private int foodSizeId;
